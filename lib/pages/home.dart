a715daa3e8b2d655412d8b8f0e4ea4f1d6ebae45
pepmeg <vip.igor.nikitin@mail.ru>
2026-03-31 20:21:54 +0300

    search

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,10 +2,23 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:untitled/utils/colors.dart';
 import '../data/product_data.dart';
+import '../models/product.dart';
 import '../widgets/product_card.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => HomeState();
+
+}
+
+class HomeState extends State<Home> {
+  String searcQuery = '';
+
+  List<Product> get filteredProducts {
+    return ProductData.searchProducts(searcQuery);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,14 +36,22 @@ class Home extends StatelessWidget {
                     borderRadius: BorderRadius.circular(30),
                   ),
                   child: TextField(
-                    onChanged: (value) {},
+                    onChanged: (value) {
+                      setState(() {
+                        searcQuery = value;
+                      });
+                    },
                     decoration: InputDecoration(
                       hintText: 'Поиск...',
                       prefixIcon: Icon(Icons.search, color: AppColors.copper,),
                       suffixIcon: IconButton(
                           icon: Icon(
                             Icons.filter_list, color: AppColors.copper,),
-                          onPressed: () {}
+                          onPressed: () {
+                            setState(() {
+                              searcQuery = '';
+                            });
+                          }
                       ),
                       border: InputBorder.none,
                       contentPadding: EdgeInsets.symmetric(
